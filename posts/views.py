483031bf854b1139c1d413db8c3c0ca3fb187a55
wadada <w.daiki1175@gmail.com>
2024-06-21 13:13:31 +0900

    first commit

---
+++ posts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
+
+def index(request):
+    # return HttpResponse("Hello, world. You're at the posts index.")
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts': posts})
+# Create your views here.
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post,pk=post_id)
+    return render(request, 'posts/post_detail.html', {'post': post})
+
+def about(request):
+    return render(request, 'posts/about.html')
